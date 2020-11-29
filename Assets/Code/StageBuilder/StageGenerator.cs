25f6982b5b0bb87ebeb5306ee54f65ade57b8cbf
daichihasegawa0923 <punyachi@yahoo.co.jp>
2020-11-30 00:08:34 +0900

    自動生成ロジック作成中

--- Assets/Code/StageBuilder/StageGenerator.cs
+++ Assets/Code/StageBuilder/StageGenerator.cs
@@ -28,7 +28,7 @@ namespace Tower.StageBuilder
         /// <remarks>
         /// 1次元目がY軸、2次元目がX軸
         /// </remarks>
-        private List<List<Stage>> _stageMap;
+        private List<List<Stage>> _stageMap = new List<List<Stage>>();
 
         // Start is called before the first frame update
         void Start()
@@ -46,6 +46,7 @@ namespace Tower.StageBuilder
             var ySize = MINIMUM_Y + level * 2;
 
             var list = new List<List<Stage>>(ySize);
+            var gList = new List<List<GameObject>>(ySize);
 
             for (int i = 0; i < ySize; i++)
             {
@@ -53,13 +54,16 @@ namespace Tower.StageBuilder
                 for (int j = 0; j < xSize; j++)
                 {
                     // 一度すべて壁にする
-                    xList[j] = new Stage(new Vector2(j, i),true,false);
+                    xList.Add(new Stage(new Vector2(j, i),true,false));
                 }
-                list[i] = xList;
+
+                list.Add(xList);
             }
 
             this._stageMap = list;
+                        
             DigLogic(this._stageMap);
+            GenerateObject(this._stageMap);
         }
 
         /// <summary>
@@ -73,18 +77,22 @@ namespace Tower.StageBuilder
             {
                 if(stage == null)
                 {
-                    var randY = (int)Random.Range(1, map.Count - 1);
-                    var randX = (int)Random.Range(1, map[randY].Count - 1);
+                    var randY = (int)Random.Range(1, map.Count - 2);
+                    var randX = (int)Random.Range(1, map[randY].Count - 2);
                     stage = map[randY][randX];
                 }
 
-                var forward = (int)Random.Range(0, 4);
-                switch(forward)
-                {
-                    case 0:
+                var next = ChooseDigLogicEachStage(stage, map);
 
-                        break;
+                if(next == null)
+                {
+                    stage = null;
+                    continue;
                 }
+
+                next.ForEach(s => { s.IsWall = false; s.IsMovable = true; });
+
+                stage = next[1];
             }
         }
 
@@ -99,13 +107,7 @@ namespace Tower.StageBuilder
                 for (int x = 0; x < map[y].Count; x++)
                 {
                     var next = ChooseDigLogicEachStage(map[y][x], map);
-                    var right = x + 2 < map[y].Count ? map[y][x + 2].IsWall : false;
-                    var left = x - 2 >= 0 ? map[y][x - 2].IsWall : false;
-                    var down = y + 2 < map.Count ? map[y + 2][x].IsWall : false;
-                    var up = y - 2 >= 0 ? map[y - 2][x].IsWall : false;
-
-                    var canDig = right || left || down || up;
-                    if (canDig)
+                    if (next != null)
                         return true;
                 }
             }
@@ -118,33 +120,50 @@ namespace Tower.StageBuilder
         /// <param name="stage">現在の位置</param>
         /// <param name="map">全体マップ</param>
         /// <returns>次の穴を掘る場所</returns>
-        private Stage ChooseDigLogicEachStage(Stage stage, List<List<Stage>> map)
+        private List<Stage> ChooseDigLogicEachStage(Stage stage, List<List<Stage>> map)
         {
-            Stage nextStage = null;
-            var kouhoList = new List<Stage>();
+            List<Stage> nextStage = new List<Stage>();
+            var kouhoList = new List<List<Stage>>();
 
             var position = stage.Position;
             var right = position.x + 2 < map[(int)position.y].Count ? map[(int)position.y][(int)position.x + 2].IsWall : false;
             if (right)
-                kouhoList.Add(map[(int)position.y][(int)position.x + 1]);
+                kouhoList.Add(new List<Stage> { map[(int)position.y][(int)position.x + 1] , map[(int)position.y][(int)position.x + 2] });
 
             var left = position.x - 2 >= 0 ? map[(int)position.y][(int)position.x - 2].IsWall : false;
             if (left)
-                kouhoList.Add(map[(int)position.y][(int)position.x - 1]);
+                kouhoList.Add(new List<Stage> { map[(int)position.y][(int)position.x - 1] , map[(int)position.y][(int)position.x - 2] });
 
             var down = position.y + 2 < map.Count ? map[(int)position.y + 2][(int)position.x].IsWall : false;
             if (down)
-                kouhoList.Add(map[(int)position.y + 1][(int)position.x]);
+                kouhoList.Add(new List<Stage> { map[(int)position.y + 2][(int)position.x], map[(int)position.y + 2][(int)position.x] });
 
             var up = position.y - 2 >= 0 ? map[(int)position.y - 2][(int)position.x].IsWall : false;
             if (up)
-                kouhoList.Add(map[(int)position.y - 1][(int)position.x]);
+                kouhoList.Add(new List<Stage> { map[(int)position.y - 1][(int)position.x], map[(int)position.y - 2][(int)position.x] });
 
             if (kouhoList.Count == 0)
                 return nextStage;
 
             nextStage = kouhoList[(int)Random.Range(0, kouhoList.Count)];
             return nextStage;
+        }
+
+        private void GenerateObject(List<List<Stage>> map)
+        {
+            foreach(var list in map)
+            {
+                foreach(var stage in list)
+                {
+                    if (!stage.IsWall)
+                        continue;
+                    var obj = GameObject.Instantiate(_wallObject);
+                    var position = obj.transform.position;
+                    position.x = stage.Position.x;
+                    position.z = stage.Position.y;
+                    obj.transform.position = position;
+                }
+            }
         }
 
         // Update is called once per frame
